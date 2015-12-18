029b3271d1ceb30db3e9e6f75550301d8042f1d4
Paul Rudmik <paul.rudmik@gmail.com>
2015-12-18 01:26:36 -0500

    Update ProbabilitySimulator.java

--- ProbabilitySimulator.java
+++ ProbabilitySimulator.java
@@ -15,133 +15,128 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
- 
+
 package probability.simulator;
+
+/**
+ *
+ * @author paulrudmik
+ */
 
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-import java.awt.event.*;
+import javax.swing.event.*;
 
-public class ProbEvent implements ActionListener, Runnable {
-    
-    ProbabilitySimulator gui;
-    Thread rolling;
-    
-    OptionsWindow opt;
-    
-    double s_prob = OptionsWindow.PSD;
-    double f_prob = OptionsWindow.PFD;
-    int trials = 0;
-    int s_count = 0;
-    int f_count = 0;
-    int max_trials = OptionsWindow.maxTrialsD;
-    
-    double[] p_vals = new double[3];
-    double[] d_p_vals = new double[3];
-    int[] i_vals = new int[3];
-    int[] d_i_vals = new int[3];
-    
-    public ProbEvent (ProbabilitySimulator in) {
-        gui = in;
-        p_vals[0] = s_prob;
-        p_vals[1] = f_prob;
-        p_vals[2] = max_trials;
-        i_vals[0] = s_count;
-        i_vals[1] = f_count;
-        i_vals[2] = trials;
-        d_p_vals = p_vals;
-        d_i_vals = i_vals;
-    }
-    
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        String command = e.getActionCommand();
-        switch (command) {
-            case "Roll":
-                startRolling();
-                break;
-            case "Stop":
-                stopRolling();
-                break;
-            case "Reset":
-                reset();
-                break;
-            case "Options":
-                startOptions();
-                break;
-            case "Okay":
-                closeOptions();
-                break;
-        }
-    }
+public class ProbabilitySimulator extends JFrame {
 
-    private void startRolling() {
-        rolling = new Thread(this);
-        rolling.start();
-        gui.stop.setEnabled(true);
-        gui.roll.setEnabled(false);
-        gui.reset.setEnabled(false);
+    // PROBABILITYSIMULATOR COMPONENTS
+        ProbEvent probSim = new ProbEvent(this);
+        int[] defVals = new int[2];
+    
+    // ROW 1 COMPONENTS
+        JPanel row1 = new JPanel();
+        JLabel probSLabel = new JLabel("P(S): ");
+        JTextField probSField = new JTextField(5);
+        JLabel probFLabel = new JLabel("P(F): ");
+        JTextField probFField = new JTextField(5);
+    
+    // ROW 2 COMPONENTS
+        JPanel row2 = new JPanel();
+        JLabel trialsLabel = new JLabel("Number of trials: ");
+        JTextField trialsField = new JTextField(2);        
+        
+    // ROW 3 COMPONENTS
+        JPanel row3 = new JPanel();
+        JButton roll = new JButton("Roll");
+        JButton stop = new JButton("Stop");
+        JButton reset = new JButton("Reset");        
+        
+    // ROW 4 COMPONENTS
+        JPanel row4 = new JPanel();
+        JLabel yieldLabel = new JLabel("Your Roll Yielded: ");
+        JTextField sNum = new JTextField(2);
+        JLabel sLabel = new JLabel("successes and ");
+        JTextField fNum = new JTextField(2);
+        JLabel fLabel = new JLabel("failures.");
+        
+    
+    // ROW 5 COMPONENTS
+        JPanel row5 = new JPanel();
+        JButton options = new JButton("Options");
+    
+    public ProbabilitySimulator() {
+        super("Binomial Simulator!");
+        
+        
+        //setSize(550, 450);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        GridLayout theGrid = new GridLayout(5, 1, 1, 1);
+        setLayout(theGrid);
+       
+        // LOTTOEVENT OBJECTS
+            stop.addActionListener(probSim);
+            roll.addActionListener(probSim);
+            reset.addActionListener(probSim);
+            options.addActionListener(probSim);
+                
+        // ROW 1 INITIALIZES
+            //FlowLayout layout1 = new FlowLayout(FlowLayout.CENTER, 1, 1);
+            //row1.setLayout(layout1);
+            row1.add(probSLabel);
+            row1.add(probSField);
+            row1.add(probFLabel);
+            row1.add(probFField);
+            probSField.setEditable(false);
+            probFField.setEditable(false);
+            add(row1);
+            
+            probSField.setText("" + probSim.s_prob);
+            probFField.setText("" + probSim.f_prob);
+        
+        // ROW 2 INTIALIZES
+            //row2.setLayout(layout1);
+            row2.add(trialsLabel);
+            row2.add(trialsField);
+            trialsField.setEditable(false);
+            add(row2);
+            
+            trialsField.setText("" + probSim.trials);
+        
+        // ROW 3 INITIALIZES
+            //row3.setLayout(layout1);
+            stop.setEnabled(false);
+            row3.add(roll);
+            row3.add(stop);
+            row3.add(reset);
+            add(row3);
+        
+        // ROW 4 INITIALIZES
+            //GridLayout layout4 = new GridLayout(2, 3, 20, 10);
+            //row4.setLayout(layout1);
+            row4.add(yieldLabel);
+            row4.add(sNum);
+            row4.add(sLabel);
+            row4.add(fNum);
+            row4.add(fLabel);
+            sNum.setEditable(false);
+            fNum.setEditable(false);
+            add(row4);
+            
+            sNum.setText("" + probSim.s_count);
+            fNum.setText("" + probSim.f_count);
+        
+        // ROW 5 INTIALIZES
+            //row5.setLayout(layout1);
+            row5.add(options);
+            add(row5);
+            
+        setSize(400, 250);
+        setVisible(true);
+    }  
+        
+    public static void main(String[] args) {
+        ProbabilitySimulator frame = new ProbabilitySimulator();
     }
     
-    private void roll() {
-        double roll = Math.random();
-        if (roll > p_vals[1]) {
-            s_count++;
-            gui.sNum.setText("" + s_count);
-        } else {
-            f_count++;
-            gui.fNum.setText("" + f_count);
-        }
-        trials++;
-        gui.trialsField.setText("" + trials);
-    }
-    
-    private void stopRolling() {
-        rolling = null;
-        gui.stop.setEnabled(false);
-        gui.roll.setEnabled(true);
-        gui.reset.setEnabled(true);        
-    }
-    
-    private void reset() {
-        gui.stop.setEnabled(false);
-        gui.roll.setEnabled(true);
-        gui.reset.setEnabled(false);
-        s_count = 0;
-        f_count = 0;
-        trials = 0;
-        gui.sNum.setText("0");
-        gui.fNum.setText("0");
-        gui.trialsField.setText("0");
-        gui.probSField.setText("" + p_vals[0]);
-        gui.probFField.setText("" + p_vals[1]);
-    }
-    
-    private void startOptions() {
-        opt = new OptionsWindow();
-        opt.okay.addActionListener(this);
-        stopRolling();
-    }
-
-    private void closeOptions() {
-        p_vals = opt.getValues();
-        OptionsWindow.PSD = p_vals[0];
-        OptionsWindow.PFD = p_vals[1];
-        OptionsWindow.maxTrialsD = (int)p_vals[2];
-        reset();
-        opt.setVisible(false);
-    }
-    
-    @Override
-    public void run() {
-        while (trials < p_vals[2] && rolling == Thread.currentThread()) {
-            roll();
-            try {
-                Thread.sleep(35);
-            } catch (InterruptedException e) {
-            }
-        }
-        rolling = null;
-    } 
 }
